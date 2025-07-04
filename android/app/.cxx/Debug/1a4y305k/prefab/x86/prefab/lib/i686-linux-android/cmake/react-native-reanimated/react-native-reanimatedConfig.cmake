59725bb08085b52f2686ac0dbdc128cd296f804e
Josue <josuepe03@hotmail.com>
2025-07-04 15:19:25 -0600

    fixed: prodcut list

---
+++ android/app/.cxx/Debug/1a4y305k/prefab/x86/prefab/lib/i686-linux-android/cmake/react-native-reanimated/react-native-reanimatedConfig.cmake
@@ -0,0 +1,18 @@
+if(NOT TARGET react-native-reanimated::reanimated)
+add_library(react-native-reanimated::reanimated SHARED IMPORTED)
+set_target_properties(react-native-reanimated::reanimated PROPERTIES
+    IMPORTED_LOCATION "/home/josue/Escritorio/mvp_app_react_native/node_modules/react-native-reanimated/android/build/intermediates/cxx/Debug/33571z4b/obj/x86/libreanimated.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/home/josue/Escritorio/mvp_app_react_native/node_modules/react-native-reanimated/android/build/prefab-headers/reanimated"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
+if(NOT TARGET react-native-reanimated::worklets)
+add_library(react-native-reanimated::worklets SHARED IMPORTED)
+set_target_properties(react-native-reanimated::worklets PROPERTIES
+    IMPORTED_LOCATION "/home/josue/Escritorio/mvp_app_react_native/node_modules/react-native-reanimated/android/build/intermediates/cxx/Debug/33571z4b/obj/x86/libworklets.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/home/josue/Escritorio/mvp_app_react_native/node_modules/react-native-reanimated/android/build/prefab-headers/worklets"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
